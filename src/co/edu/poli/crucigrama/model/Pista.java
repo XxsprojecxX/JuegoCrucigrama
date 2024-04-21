20b0c2692db7a93d910eee84fd3bf169d2910ba0
apineda9787 <andrespineda.9704@gmail.com>
2024-04-21 15:52:20 -0500

    Se realiza ajustes para el patron MVC

---
+++ src/co/edu/poli/crucigrama/model/Pista.java
@@ -0,0 +1,5 @@
+package co.edu.poli.crucigrama.model;
+
+public class Pista {
+
+}
--- src/co/edu/poli/crucigrama/view/VistaImplementacion.java
+++ src/co/edu/poli/crucigrama/view/VistaImplementacion.java
@@ -1,18 +1,17 @@
-package application;
-	
+package co.edu.poli.crucigrama.view;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
 
-public class Main extends Application {
+public class VistaImplementacion extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("crucigrama.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("cruci2.fxml"));
 			Scene scene = new Scene(root);
 			primaryStage.setTitle("CRUCIGRAMA");
 			primaryStage.setScene(scene);
