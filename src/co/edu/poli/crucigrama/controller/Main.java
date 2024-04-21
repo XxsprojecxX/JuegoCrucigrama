118dda8b3eabd68e46a01fc64f570f6a729a59ef
apineda9787 <andrespineda.9704@gmail.com>
2024-04-21 16:48:48 -0500

    Ajustes al codigo Main

--- src/co/edu/poli/crucigrama/controller/Main.java
+++ src/co/edu/poli/crucigrama/controller/Main.java
@@ -1,9 +1,13 @@
 package co.edu.poli.crucigrama.controller;
 
+import co.edu.poli.crucigrama.view.VistaImplementacion;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
+		VistaImplementacion vista = new VistaImplementacion();
+		vista.start(null);
 
 	}
 
